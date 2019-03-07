a8c3f633eac216a48dcf0e4a82092fb33a863cf4
Rodny Molina <rodnymolina@gmail.com>
2019-03-06 21:26:41 -0800

    Add basic file-system operations

--- dir.go
+++ dir.go
@@ -2,28 +2,154 @@ package main
 
 import (
 	"context"
+	"fmt"
+	"io/ioutil"
 	"log"
+	"os"
+	"path/filepath"
+	"syscall"
 
 	"bazil.org/fuse"
+	"bazil.org/fuse/fs"
 )
 
 //
-// Dir struct serves as a FUSE-friendly abstraction to represent file-directories
+// Dir struct serves as a FUSE-friendly abstraction to represent directories
 // present in the host FS.
 //
 type Dir struct {
-	// TODO: Populate needed fields. Think about must-have requirements.
-	path string
+	//
+	// Underlying File struct representing each directory.
+	//
+	File
+
+	//
+	// TODO: Think about the need to define virtual-folders structs and its
+	// associated logic. If there's no such a need, and there's no
+	// differentiated logic between Dir and File structs, then consider the
+	// option of consolidating all associated logic within a single
+	// abstraction.
+	//
 }
 
 //
-// Attr FS instruction
+// NewDir method serves as Dir constructor.
 //
-func (d *Dir) Attr(ctx context.Context, a *fuse.Attr) error {
+func NewDir(path string, attr *fuse.Attr) *Dir {
 
-	log.Println("Requested Attr for directory", d.path)
+	newDir := &Dir{
+		File: *NewFile(path, attr),
+	}
 
-	// TODO: "a" should be modified before returning.
+	return newDir
+}
 
-	return nil
+//
+// Lookup FS operation.
+//
+func (d *Dir) Lookup(
+	ctx context.Context,
+	req *fuse.LookupRequest,
+	resp *fuse.LookupResponse) (fs.Node, error) {
+
+	log.Println("Requested Lookup for", req.Name)
+
+	path := filepath.Join(d.path, req.Name)
+
+	// Return right away if the resource being queried is not available.
+	info, err := os.Stat(path)
+	if err != nil {
+		fmt.Println("No directory", path, "found in FS")
+		return nil, fuse.ENOENT
+	}
+
+	if info.IsDir() {
+		log.Println("Found match for directory lookup with size", info.Size())
+
+		//
+		// Obtaining FS directory attributes. Notice that directory 'mode' must
+		// be explicitly defined.
+		//
+		attr := StatToAttr(info.Sys().(*syscall.Stat_t))
+		attr.Mode = os.ModeDir | attr.Mode
+
+		return NewDir(path, &attr), nil
+
+	}
+
+	log.Println("Found match for file lookup with size", info.Size())
+
+	// Obtaining FS file attributes
+	attr := StatToAttr(info.Sys().(*syscall.Stat_t))
+
+	return NewFile(path, &attr), nil
+}
+
+//
+// Open FS operation.
+//
+func (d *Dir) Open(
+	ctx context.Context,
+	req *fuse.OpenRequest,
+	resp *fuse.OpenResponse) (fs.Handle, error) {
+
+	_, err := d.File.Open(ctx, req, resp)
+	if err != nil {
+		return nil, err
+	}
+
+	return d, nil
+}
+
+//
+// ReadDirAll FS operation.
+//
+func (d *Dir) ReadDirAll(ctx context.Context) ([]fuse.Dirent, error) {
+
+	var children []fuse.Dirent
+
+	log.Println("Requested ReadDirAll on directory", d.path)
+
+	files, err := ioutil.ReadDir(d.path)
+	if err != nil {
+		fmt.Println("Error while running ReadDirAll(): ", err)
+		return nil, err
+	}
+
+	for _, node := range files {
+		//
+		// For system's root dir ("/"), we will only take into account
+		// the specific paths emulated by Sysvisorfs.
+		//
+		if d.path == "/" {
+			if node.Name() != "sys" && node.Name() != "proc" {
+				continue
+			}
+		}
+
+		elem := fuse.Dirent{Name: node.Name()}
+
+		if node.IsDir() {
+			elem.Type = fuse.DT_Dir
+		} else if node.Mode().IsRegular() {
+			elem.Type = fuse.DT_File
+		}
+
+		children = append(children, elem)
+	}
+
+	return children, nil
+}
+
+//
+// Mkdir FS operation
+//
+func (d *Dir) Mkdir(ctx context.Context, req *fuse.MkdirRequest) (fs.Node, error) {
+
+	log.Println("Requested Mkdir() for directory", req.Name)
+
+	path := filepath.Join(d.path, req.Name)
+	newDir := NewDir(path, &fuse.Attr{})
+
+	return newDir, nil
 }
--- fs.go
+++ fs.go
@@ -3,7 +3,10 @@ package main
 import (
 	"fmt"
 	"os"
+	"syscall"
+	"time"
 
+	"bazil.org/fuse"
 	"bazil.org/fuse/fs"
 )
 
@@ -23,7 +26,7 @@ type SysvisorFS struct {
 //
 func NewSysvisorFS(path string) *SysvisorFS {
 
-	_, err := os.Stat(path)
+	info, err := os.Stat(path)
 	if err != nil {
 		fmt.Println("File-System path \"", path, "\" not found")
 		return nil
@@ -35,12 +38,52 @@ func NewSysvisorFS(path string) *SysvisorFS {
 		size: 0,
 	}
 
+	//
+	// Creating a first node corresponding to the root element in
+	// sysvisorFS.
+	//
+	attr := StatToAttr(info.Sys().(*syscall.Stat_t))
+	attr.Mode = os.ModeDir | os.FileMode(int(0777))
+	newfs.root = NewDir(path, &attr)
+
 	return newfs
 }
 
 //
-// Root method. This is a FUSE lib requirement. Function returns FS root-node.
+// Root method. This is a FUSE-lib requirement. Function returns sysvisor-fs'
+// root-node.
 //
 func (f *SysvisorFS) Root() (fs.Node, error) {
 	return f.root, nil
+}
+
+//
+// StatToAttr helper function to translate FS node-parameters from unix/kernel
+// format to FUSE ones.
+//
+// Kernel FS node attribs:  fuse.attr (fuse_kernel*.go)
+// FUSE node attribs:       fuse.Attr (fuse.go)
+//
+// TODO: Place me in a more appropiate location
+//
+func StatToAttr(s *syscall.Stat_t) fuse.Attr {
+
+	var a fuse.Attr
+
+	a.Inode = uint64(s.Ino)
+	a.Size = uint64(s.Size)
+	a.Blocks = uint64(s.Blocks)
+
+	a.Atime = time.Unix(s.Atim.Sec, s.Atim.Nsec)
+	a.Mtime = time.Unix(s.Mtim.Sec, s.Mtim.Nsec)
+	a.Ctime = time.Unix(s.Ctim.Sec, s.Ctim.Nsec)
+
+	a.Mode = os.FileMode(s.Mode)
+	a.Nlink = uint32(s.Nlink)
+	a.Uid = uint32(s.Uid)
+	a.Gid = uint32(s.Gid)
+	a.Rdev = uint32(s.Rdev)
+	a.BlockSize = uint32(s.Blksize)
+
+	return a
 }
